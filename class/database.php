954b21288f3d8dcd00db75c370953a77531b0fd7
budiali <albab080808@gmail.com>
2022-04-11 23:58:38 +0700

    update connection db

--- class/database.php
+++ class/database.php
@@ -9,18 +9,32 @@ class Database{
     
     function __construct()
     {
-        $koneksi = mysqli_connect($this->host, $this->uname, $this->password);
-        mysqli_select_db($koneksi, $this->db);
-
+        $koneksi = $this->connection_db();
         if($koneksi){
-            echo "Database berhasil terhubung";
+            echo "Database berhasil terhubung!";
         }else{
             echo "Database gagal terhubung";
         }
     }
 
+    function connection_db(){
+        // static $conn;
+        $conn = mysqli_connect($this->host, $this->uname, $this->password);
+        mysqli_select_db($conn, "malasngoding");
+        return $conn;
+    }
+
+    function ambil_data(){
+        $koneksi = $this->connection_db();
+        $data = mysqli_query($koneksi,"Select * from user");
+        while ($d = mysqli_fetch_array($data)) {
+            $hasil[] = $d;
+        }
+        return $hasil;
+    }
+
 }
 
-$newDb = new Database();
+// $newDb = new Database();
 
 ?>
