45fa2e3885ef24e7cfda26947e469cd3d7cdbb9f
budiali <albab080808@gmail.com>
2022-04-11 07:51:08 +0700

    added file destruct

---
+++ class/destruct.php
@@ -0,0 +1,32 @@
+<?php 
+
+class classConstruct{
+
+    var $nama;
+
+    //__construct adalah function yang pertama kali dijalankan!
+    // tidak perlu memanggil lagi func __construct pada saat instantiate
+    function __construct()
+    {
+        echo "Ini adalah pesan function __construct!<br>";
+    }
+
+    /*
+    jika __construct yang pertama kali dijalankan
+    __destruct kebalikannya, yaitu yang paling akhir dijalankan
+    */
+    function __destruct()
+    {
+        echo "Ini adalah pesan function __destruct!<br>";
+    }
+
+    function tampilNama(){
+        echo "Nama saya Ulil Albab <br>";
+    }
+
+}
+
+$instantiate = new classConstruct();
+$instantiate->tampilNama();
+
+?>
